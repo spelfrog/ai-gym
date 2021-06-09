f9dd92abfa334c6caa631dc5539ab171d3b53ca3
spelfrog <33190153+spelfrog@users.noreply.github.com>
2021-06-09 17:25:33 +0200

    Adding packet metadata

--- setup.py
+++ setup.py
@@ -2,5 +2,7 @@ from setuptools import setup
 
 setup(name='fh_ac_ai_gym',
       version='0.0.1',
-      install_requires=['gym','numpy']
+      install_requires=['gym','numpy'],
+      packages=setuptools.find_packages(),
+      include_package_data=True,
       )
